329b87628160316e645298f895367025d9be6987
shalini-polapally <37867151+shalini-polapally@users.noreply.github.com>
2022-11-16 20:21:51 +0530

    configuration

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,19 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    // implement node event listeners here
+    baseUrl: "",
+    watchForFileChanges: false,
+    viewportWidth: 1000,
+    viewportHeight: 660,
+    waitForAnimations: true,
+    animationDistanceThreshold: 20,
+    defaultCommandTimeout: 5000,
+    execTimeout: 60000,
+    pageLoadTimeout: 60000,
+    requestTimeout: 15000,
+    responseTimeout: 15000,
+    video: false,
+    failOnStatusCode: false
   }
 });
