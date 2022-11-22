7f7a250f413b8c5c27bc4d58fdcebdbcd4eb7a59
shalini-polapally <polapallyshalini1@gmail.com>
2022-11-22 08:37:16 +0000

    new tests

---
+++ cypress/e2e/examples/e2e/payment.cy.js
@@ -0,0 +1,25 @@
+describe('New-payee test', () => {
+    before(function () {
+        cy.visit('http://zero.webappsecurity.com/index.html')
+        cy.url().should('include', 'index.html')
+        cy.get('#signin_button').click()
+        cy.fixture('user').then(user => {
+            const username = user.id
+            const password = user.pwd
+            cy.login(username, password)
+        })
+    })
+
+    it('Should add payment', () => {
+        //TODO
+        cy.contains('Pay Bills').click()
+        cy.contains('Pay Saved Payee').click()
+        cy.get('#sp_payee').select('Sprint')
+        cy.get('#sp_account').select('Checking')
+        cy.get('#sp_amount').type(1000)
+        cy.get('#sp_date').type('2022-11-02 {enter}')
+        cy.get('#sp_description').type('savings')
+        cy.get('#pay_saved_payees').click()
+        cy.get('#alert_content').should('be.visible')
+    })
+})
---
+++ cypress/e2e/examples/e2e/purchase.cy.js
@@ -0,0 +1,24 @@
+describe('New-payee test', () => {
+    before(function () {
+        cy.visit('http://zero.webappsecurity.com/index.html')
+        cy.url().should('include', 'index.html')
+        cy.get('#signin_button').click()
+        cy.fixture('user').then(user => {
+            const username = user.id
+            const password = user.pwd
+            cy.login(username, password)
+        })
+    })
+
+    it('Should add payment', () => {
+        //TODO
+        cy.contains('Pay Bills').click()
+        cy.contains('Purchase Foreign Currency').click()
+        cy.get('#pc_currency').select('Australia (dollar)')
+        cy.get('#pc_amount').type(1000)
+        cy.get('#pc_inDollars_false').click()
+        cy.get('#pc_calculate_costs').click()
+        cy.get('#purchase_cash').click()
+        cy.get('#alert_content').should('be.visible')
+    })
+})
